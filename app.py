53adc0474cbb1cb2884d4bb0802b85e503343b54
Rian Oleary <rianoleary@Rians-MacBook-Pro.local>
2025-11-05 19:39:07 +0000

    practicing with flask routes, arguments and parameters. Also set up a venv for better package management

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-  greeting = "hello world again"
-  return_html = "<html><body>" + greeting + "</body></html>"
-  return return_html
+  return render_template ("index.html")
+
+@app.route("/notes")
+def notes():
+  return render_template ("notes.html")
+
+@app.route("/notes/<date>")
+def notes_today(date):
+  return "<html><body><h4>You're notes for " + str(date) + " are available </h4></body></html>"
 
 if __name__ == "__main__":
   app.run(debug = True)
+  
